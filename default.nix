9b696a005277ae8b49f7aa53fa3bc54b9e041d73
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-08-13 00:38:00 -0600

    Split default.nix into release.nix
    Now release runs against a specific version of nixpkgs, while the package can
    be called by other people.

--- default.nix
+++ default.nix
@@ -1,20 +1,24 @@
-let
+# This package can be called with the nixpkg function
+# `agdaPackages.callPackage`, which is where the `standard-library` input comes
+# from.
+{ lib, stdenv, mkDerivation, standard-library }:
 
-  pkgs = import (builtins.fetchTarball {
-    url =
-      "https://github.com/NixOS/nixpkgs-channels/archive/32b46dd897ab2143a609988a04d87452f0bbef59.tar.gz";
-    sha256 = "1gzfrpjnr1bz9zljsyg3a4zrhk8r927sz761mrgcg56dwinkhpjk";
-  }) { };
-
-in pkgs.agdaPackages.mkDerivation {
+mkDerivation {
   version = "0.1";
   pname = "functional-linear-algebra";
 
-  buildInputs = [ pkgs.agdaPackages.standard-library ];
+  buildInputs = [ standard-library ];
 
-  src = pkgs.lib.sourceFilesBySuffices ./. [ ".agda" ".agda-lib" ".lagda" ".lagda.md" ".lagda.tex" ".lagda.rst" ];
+  src = lib.sourceFilesBySuffices ./. [
+    ".agda"
+    ".lagda"
+    ".lagda.md"
+    ".lagda.rst"
+    ".lagda.tex"
+    ".agda-lib"
+  ];
 
-  meta = with pkgs.stdenv.lib; {
+  meta = with stdenv.lib; {
     homepage = "https://github.io/ryanorendorff/functional-linear-algebra";
     description = ''
       Formalizing linear algebra in Agda by representing matrices as functions
