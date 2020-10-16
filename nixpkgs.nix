6d55b97ca94286fcd2b122c273b3fe6cb29bc36b
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-10-16 14:57:08 -0600

    Use derivation from nixpkgs instead of redefining
    Since the derivation is already specified in nixpkgs, we simply override
    the source to be the local source.

---
+++ nixpkgs.nix
@@ -0,0 +1,7 @@
+# nixpkgs unstable channel from October 15th, 2020
+import (builtins.fetchTarball {
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/dfd0a64c1a25150092e1e6200fdc59da309466e5.tar.gz";
+  sha256 = "1aadmhqd4d02kp7gk5r83r0p8d6n9qj1w7xlh0argi09msvamjcw";
+}) { }
+
--- release.nix
+++ release.nix
@@ -1,10 +1,1 @@
-let
-
-  # nixpkgs unstable channel from Aug 10 2020, 11:11 AM GMT
-  pkgs = import (builtins.fetchTarball {
-    url =
-      "https://github.com/NixOS/nixpkgs-channels/archive/32b46dd897ab2143a609988a04d87452f0bbef59.tar.gz";
-    sha256 = "1gzfrpjnr1bz9zljsyg3a4zrhk8r927sz761mrgcg56dwinkhpjk";
-  }) { };
-
-in pkgs.agdaPackages.callPackage ./default.nix { }
+let pkgs = import ./nixpkgs.nix; in import ./default.nix { inherit pkgs; }
