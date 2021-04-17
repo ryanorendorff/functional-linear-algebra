38122d51fa1d4c6e7ae022656b3bc496dd573bc8
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2021-04-16 20:13:27 -0600

    Update nixpkgs where stdlib 1.5 is standard

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,7 @@
-# nixpkgs unstable channel from November 25th, 2020
+# nixpkgs unstable channel from April 16th, 2021
 import (builtins.fetchTarball {
   url =
-    "https://github.com/NixOS/nixpkgs/archive/9af5a55a1960fc31f2f671419a28d4a65ac18dfb.tar.gz";
-  sha256 = "1hxdcggxkz2a8sc31hs7ka0fjbp4a90rkgfr8x629mm5w9pzniws";
+    "https://github.com/NixOS/nixpkgs/archive/e5cc06a1e806070693add4f231060a62b962fc44.tar.gz";
+  sha256 = "04543i332fx9m7jf6167ac825s4qb8is0d0x0pz39il979mlc87v";
 }) { }
 
