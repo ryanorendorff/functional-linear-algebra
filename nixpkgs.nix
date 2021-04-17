c2f76dcfa12e9fc1f41f6f5acdf3404f61cddb81
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2021-04-17 08:58:49 -0600

    Standard-library 1.6-rc1 (#36)
    * overlay standard-library 1.6-rc1
    
    * Enable overlay of stdlib using bool
    
    * README directions for overriding standard-library

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,22 @@
 # nixpkgs unstable channel from April 16th, 2021
-import (builtins.fetchTarball {
+
+let
+
+  # Temporary workaround while Agda standard library is an RC.
+  agda-standard-library-1-6-overlay = (self: super: {
+    agdaPackages = super.agdaPackages // {
+      standard-library = super.agdaPackages.standard-library.overrideAttrs
+        (old: import ./agda-stardard-library.nix super);
+
+      functional-linear-algebra =
+        super.agdaPackages.functional-linear-algebra.override ({
+          standard-library = self.agdaPackages.standard-library;
+        });
+    };
+  });
+
+in import (builtins.fetchTarball {
   url =
     "https://github.com/NixOS/nixpkgs/archive/e5cc06a1e806070693add4f231060a62b962fc44.tar.gz";
   sha256 = "04543i332fx9m7jf6167ac825s4qb8is0d0x0pz39il979mlc87v";
-}) { }
-
+}) { overlays = [ agda-standard-library-1-6-overlay ]; }
