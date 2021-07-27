8c0a2d2f95a0a9349f7e3b0fb4b60a9022bf261f
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2021-07-26 19:23:23 -0600

    Remove upper bound on agda-stdlib (1.7 compatible)
    The upper bound mostly caused some issues related to the nixpkgs moving faster
    than this package. Removing the  upper bound so ideally nixpkgs does not need
    to be updated as often.

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 { pkgs }:
 
 pkgs.agdaPackages.functional-linear-algebra.overrideAttrs (oldAttrs: rec {
-  version = "0.3";
+  version = "0.4";
 
   src = pkgs.lib.sourceFilesBySuffices ./. [
     ".agda"
@@ -18,4 +18,8 @@ pkgs.agdaPackages.functional-linear-algebra.overrideAttrs (oldAttrs: rec {
     # Find all .agda files in the src/ directory and put it in Everything.agda
     ./generate-everything.sh
   '';
+
+  # Force the meta to be unbroken so that the package will always attempt to
+  # build in CI, regardless of the upstream broken status.
+  meta.broken = false;
 })
