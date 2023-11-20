632e7483bdc3240cda66a570ed2e9044af600692
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2023-11-19 20:15:38 -0700

    flake nixpkgs pegged to master; update CI

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/haskell-updates";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
